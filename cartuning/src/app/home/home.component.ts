0c9ee7c386123455e8d10c0ad26644f0bd8fffca
mrbear96 <andrei_bublea@yahoo.com>
2020-07-05 14:54:31 +0300

    added car logo carousel, small css improvements

--- cartuning/src/app/home/home.component.ts
+++ cartuning/src/app/home/home.component.ts
@@ -51,4 +51,47 @@ export class HomeComponent implements OnInit {
     nav: false
   }
 
+  brandCarouselOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    autoplay: true,
+    smartSpeed: 300,
+    autoplayTimeout: 10000,
+    navText: ['<', '>'],
+    responsive: {
+      1400: {
+        items: 8
+      },
+      800: {
+        items: 6
+      }
+    },
+    nav: false
+  }
+
+
+carLogoPaths = ['../../assets/car-brands/alfaromeo_logo_thumbnail.png' ,
+'../../assets/car-brands/audi_logo_thumbnail.png' ,
+'../../assets/car-brands/bmw_logo_thumbnail.png' ,
+'../../assets/car-brands/citroen_logo_thumbnail.png' ,
+'../../assets/car-brands/dacia_logo_thumbnail.png' ,
+'../../assets/car-brands/fiat_logo_thumbnail.png' ,
+'../../assets/car-brands/ford_logo_thumbnail.png' ,
+'../../assets/car-brands/lexus_logo_thumbnail.png' ,
+'../../assets/car-brands/mazda_logo_thumbnail.png' ,
+'../../assets/car-brands/mercedes_logo_thumbnail.png' ,
+'../../assets/car-brands/mitsubishi_logo_thumbnail.png' ,
+'../../assets/car-brands/nissan_logo_thumbnail.png' ,
+'../../assets/car-brands/opel_logo_thumbnail.png' ,
+'../../assets/car-brands/peugeot_logo_thumbnail.png' ,
+'../../assets/car-brands/renault_logo_thumbnail.png' ,
+'../../assets/car-brands/skoda_logo_thumbnail.png' ,
+'../../assets/car-brands/volkswagen_logo_thumbnail.png' ,
+'../../assets/car-brands/volvo_logo_thumbnail.png' ,]
+
+          
+
 }
